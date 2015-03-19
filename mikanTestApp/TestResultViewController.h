368906825ee6679f5bb646290f1a2e18c0efcc26
usatie <usatie@gmail.com>
2015-03-19 20:47:13 +0900

    [add] TestResultViewController

---
+++ mikanTestApp/TestResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestResultViewController.h
+//  mikanTestApp
+//
+//  Created by Shun Usami on 2015/03/19.
+//  Copyright (c) 2015年 ShunUsami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestResultViewController : UIViewController
+
+@end
